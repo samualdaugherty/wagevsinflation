bdd662b6597068b2f4e00f3f30ca2aef89aa2b57
Samual Daugherty <samualdaugherty@Samuals-MacBook-Pro.local>
2025-04-21 22:49:09 -0500

    Update Vercel and Vite config for production

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,12 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/wagevsinflation/',
+  build: {
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
+  }
 })
